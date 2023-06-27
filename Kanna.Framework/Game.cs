d7e93a359ae985f9b148c00af1e0e0bf1fc8b1ca
Phawit Pornwattanakul <me@helloyeew.dev>
2023-06-28 01:43:38 +0700

    🎮 Add global shortcut

--- Kanna.Framework/Game.cs
+++ Kanna.Framework/Game.cs
@@ -1,7 +1,11 @@
 ﻿using System.Reflection;
 using Kanna.Framework.Audio;
 using Kanna.Framework.Logging;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace Kanna.Framework
 {
@@ -15,6 +19,60 @@ namespace Kanna.Framework
 
             Logger.AddHeader();
             AudioManager.InitBass();
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(Color4.MediumPurple);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            SwapBuffers();
+        }
+
+        protected override void OnKeyDown(KeyboardKeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            // Control + F11 = Toggle Fullscreen
+            if (e is {Control: true, Key: Keys.F11})
+            {
+                if (WindowState == WindowState.Fullscreen)
+                    WindowState = WindowState.Normal;
+                else
+                    WindowState = WindowState.Fullscreen;
+                Logger.Log($"Window state changed to {WindowState} ({ClientSize.X}x{ClientSize.Y})");
+            }
+
+            // Control + F12 = Toggle VSync
+            // TODO: Add more FPS options
+            if (e is {Control: true, Key: Keys.F12})
+            {
+                if (VSync == VSyncMode.On)
+                    VSync = VSyncMode.Off;
+                else
+                    VSync = VSyncMode.On;
+                Logger.Log($"VSync changed to {VSync} ({ClientSize.X}x{ClientSize.Y})");
+            }
+        }
+
+        protected override void OnFocusedChanged(FocusedChangedEventArgs e)
+        {
+            base.OnFocusedChanged(e);
+
+            if (e.IsFocused)
+                // Don't limit FPS when focused
+                UpdateFrequency = 0;
+            else
+                // Limit FPS to 60 when not focused
+                UpdateFrequency = 60;
         }
     }
 }
