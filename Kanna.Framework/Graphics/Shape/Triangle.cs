f9c939073c97c20858ca94c92ad386e2eb334b29
Phawit Pornwattanakul <me@helloyeew.dev>
2023-07-02 04:09:48 +0700

    Remove unused constructor

--- Kanna.Framework/Graphics/Shape/Triangle.cs
+++ Kanna.Framework/Graphics/Shape/Triangle.cs
@@ -4,11 +4,6 @@ namespace Kanna.Framework.Graphics.Shape
 {
     public class Triangle : IDrawable
     {
-        public Triangle(float[] vertices)
-        {
-            Vertices = vertices;
-        }
-
         public float[] Vertices { get; set; }
         public Color4 Color { get; set; }
     }
