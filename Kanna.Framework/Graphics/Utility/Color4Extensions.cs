c0f1d1c0ef3f5b60796f4588f2945e67f271ef04
Phawit Pornwattanakul <me@helloyeew.dev>
2023-07-05 00:40:42 +0700

    Add Color4 utility

---
+++ Kanna.Framework/Graphics/Utility/Color4Extensions.cs
@@ -0,0 +1,32 @@
+﻿using OpenTK.Mathematics;
+
+namespace Kanna.Framework.Graphics.Utility
+{
+    public class Color4Extensions
+    {
+        /// <summary>
+        /// Converts a hex string to a <see cref="Color4"/> object.
+        /// </summary>
+        /// <param name="hex">Color in hex format.</param>
+        /// <returns><see cref="Color4"/> object.</returns>
+        public static Color4 FromHex(string hex)
+        {
+            if (hex[0] == '#')
+            {
+                hex = hex.Substring(1);
+            }
+
+            return hex.Length switch
+            {
+                6 => new Color4(byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber),
+                    byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber),
+                    byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber), 255),
+                8 => new Color4(byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber),
+                    byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber),
+                    byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber),
+                    byte.Parse(hex.Substring(6, 2), System.Globalization.NumberStyles.HexNumber)),
+                _ => throw new InvalidCastException("Invalid hex string")
+            };
+        }
+    }
+}
